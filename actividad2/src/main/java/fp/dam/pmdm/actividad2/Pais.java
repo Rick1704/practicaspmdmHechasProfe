0eedbaf53193917d3f26800464c9ba6a0a946e65
Rick1704 <ricardosf00@educastur.es>
2022-10-25 19:39:09 +0200

    empezar

---
+++ actividad2/src/main/java/fp/dam/pmdm/actividad2/Pais.java
@@ -0,0 +1,37 @@
+package fp.dam.pmdm.actividad2;
+
+public class Pais {
+    public Pais(String nombre, String capital, String continente) {
+        this.nombre = nombre;
+        this.capital = capital;
+        this.continente = continente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public void setCapital(String capital) {
+        this.capital = capital;
+    }
+
+    public String getContinente() {
+        return continente;
+    }
+
+    public void setContinente(String continente) {
+        this.continente = continente;
+    }
+
+    private String nombre;
+    private String capital;
+    private String continente;
+}
